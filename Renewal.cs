50d27c87996cc616f8b99dbfafd750e30d588968
Morgan <morgwilliams7@gmail.com>
2025-12-22 20:55:35 -0600

    Add files via upload

---
+++ Renewal.cs
@@ -0,0 +1,30 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+daysUntilExpiration = 11;
+
+if (daysUntilExpiration <= 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine("You subscription expires within a day!");
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else if (daysUntilExpiration > 10)
+{
+    Console.WriteLine(" ");
+}
