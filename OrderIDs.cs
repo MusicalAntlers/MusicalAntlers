2706b5a8734ee9fe0493569b41b6b9a469f7c901
Morgan <morgwilliams7@gmail.com>
2025-12-29 21:17:04 -0600

    Create OrderIDs.cs

---
+++ OrderIDs.cs
@@ -0,0 +1,10 @@
+using System.Data.Common;
+
+string[] fraudulentOrderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+foreach (string id in fraudulentOrderIDs)
+{
+    if (id.StartsWith("B"))
+    {
+        Console.WriteLine(id);
+    }
+}
